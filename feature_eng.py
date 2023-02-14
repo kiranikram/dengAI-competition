be8f37c7744e0b1594f30f3d279230aaa1217e67
kiranikram <kiran.ikram91@gmail.com>
2023-02-14 12:23:09 +0000

    feature imp

--- feature_eng.py
+++ feature_eng.py
@@ -1,1 +1,33 @@
+import pandas as pd 
+
 #mmake some lags 
+def add_4_rolling_means(df):
+
+    df2 = df.rolling(window=4).mean()
+    df2 = df2.rename(columns={x: x + '_4' for x in df2.columns})
+    df_all = pd.merge(df,df2, how='left',on=['year','weekofyear'])
+
+    return df_all
+
+def add_8_rolling_means(df):
+
+    df2 = df.rolling(window=8).mean()
+    df2 = df2.rename(columns={x: x + '_8' for x in df2.columns})
+    df_all = pd.merge(df,df2, how='left',on=['year','weekofyear'])
+
+    return df_all
+
+
+def  add_previous_max(df):
+    df2 = df.rolling(window=4).max()
+    df2 = df2.rename(columns={x: x + '_max' for x in df2.columns})
+    df_all = pd.merge(df,df2, how='left',on=['year','weekofyear'])
+
+    return df_all
+
+def add_dengue_lags(df):
+
+    df['prev_avg_cases_4'] = df['total_cases'].rolling(window=4).mean()
+    df['prev_avg_cases_8'] = df['total_cases'].rolling(window=8).mean()
+
+    return df
--- given_eda.py
+++ given_eda.py
@@ -1,14 +1,11 @@
 import pandas as pd
 
-path_to_features = "/Users/ikram/Desktop/dengAI-competition/data/dengue_features_train.csv"
-path_to_labels = "/Users/ikram/Desktop/dengAI-competition/data/dengue_labels_train.csv"
+path_to_features = "/Users/mussayababbasshamsi/Desktop/Kiran/dengAI-competition/data2/dengue_features_train.csv"
+path_to_labels = "/Users/mussayababbasshamsi/Desktop/Kiran/dengAI-competition/data2/dengue_labels_train.csv"
 
 def get_train_dfs():
-    sj_train , iq_train = preprocess_data(path_to_features, labels_path = path_to_labels)
-    return sj_train , iq_train
-
-
-
+    train_df = preprocess_data(path_to_features, labels_path = path_to_labels)
+    return train_df
 
 
 def preprocess_data(data_path, labels_path=None):
@@ -35,21 +32,13 @@ def preprocess_data(data_path, labels_path=None):
         labels = pd.read_csv(labels_path, index_col=[0, 1, 2])
         df = df.join(labels)
     
-    # separate san juan and iquitos
-    #TO DO access this from function below 
-    sj = df.loc['sj']
-    iq = df.loc['iq']
-    
-    return sj, iq
+    return df
 
 def split_data_by_city(df):
     sj = df.loc['sj']
     iq = df.loc['iq']
 
     return sj , iq
-
-
-sj,iq = get_train_dfs()
 
 def add_4_rolling_means(df):
 
@@ -59,28 +48,9 @@ def add_4_rolling_means(df):
 
     return df_all
 
-def add_8_rolling_means(df):
-
-    df2 = df.rolling(window=8).mean()
-    df2 = df2.rename(columns={x: x + '_8' for x in df2.columns})
-    df_all = pd.merge(df,df2, how='left',on=['year','weekofyear'])
-
-    return df_all
 
 
-def  add_previous_max(df):
-    df2 = df.rolling(window=4).max()
-    df2 = df2.rename(columns={x: x + '_max' for x in df2.columns})
-    df_all = pd.merge(df,df2, how='left',on=['year','weekofyear'])
 
-    return df_all
-
-def add_dengue_lags(df):
-
-    df['prev_avg_cases_4'] = df['total_cases'].rolling(window=4).mean()
-    df['prev_avg_cases_8'] = df['total_cases'].rolling(window=8).mean()
-
-    return df
 
     
    
